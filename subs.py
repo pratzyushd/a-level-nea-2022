8f1642c2e5365d6c1ba62217cd04a88ee316a33d
Pratyush Dwivedi <pratzyushd@gmail.com>
2022-03-27 19:23:03 +0400

    Fix function annotations for subs.py file

--- subs.py
+++ subs.py
@@ -1,7 +1,7 @@
 import codecs
-from typing import Union
+from typing import Union, Optional
 
-def decode_eth_header(header_contents: str) -> Union[dict,None]:
+def decode_eth_header(header_contents: str) -> Optional[dict]:
     """Breaks up string containing ethernet header into the various components
     of the header. This is done with a dictionary, where the key is the
     descriptor and the value is the section of the header that matches.
@@ -10,7 +10,7 @@ def decode_eth_header(header_contents: str) -> Union[dict,None]:
         header_contents (str): String containing header contents
 
     Returns:
-        Union[dict,None] : Dictionary containing split headers into sections. This
+        Optional[dict] : Dictionary containing split headers into sections. This
         may also be a None returned if the header length is found to not
         be sufficient.
     """
@@ -24,7 +24,7 @@ def decode_eth_header(header_contents: str) -> Union[dict,None]:
     else:
         return None
 
-def decode_ip_header(header_contents: str) -> Union[dict,None]:
+def decode_ip_header(header_contents: str) -> Optional[dict]:
     """ Breaks up a string containing the IP header into the various sections
     using a dictionary. This also decodes certain values (i.e. converts them
     to the character / decimal equivalents appropriately) using a separate
@@ -34,7 +34,7 @@ def decode_ip_header(header_contents: str) -> Union[dict,None]:
         header_contents (str): String containing header contents
 
     Returns:
-        Union[dict,None]: Dictionary containing headers split into sections. If
+        Optional[dict]: Dictionary containing headers split into sections. If
         the header is invalid, this will be None.
     """
     if len(header_contents) >= 40:
@@ -61,7 +61,7 @@ def decode_ip_header(header_contents: str) -> Union[dict,None]:
     else:
         return None
 
-def decode_tcp_header(header_contents: str) -> Union[dict,None]:
+def decode_tcp_header(header_contents: str) -> Optional[dict]:
     """Breaks up a string containing the TCP header in various sections using a
     dictionary. Also decodes certain values using a separate decoding function.
 
@@ -69,7 +69,7 @@ def decode_tcp_header(header_contents: str) -> Union[dict,None]:
         header_contents (str): String containing header contents
 
     Returns:
-        Union[dict,None]: Dictionary containing header split into sections. If
+        Optional[dict]: Dictionary containing header split into sections. If
         header is invalid, this will be a None.
     """
     if len(header_contents) >= 64:
@@ -89,14 +89,14 @@ def decode_tcp_header(header_contents: str) -> Union[dict,None]:
     else:
         return None
 
-def decode_http_data(packet_contents: str) -> Union[dict,None]:
+def decode_http_data(packet_contents: str) -> Optional[dict]:
     """Creates a dictionary containing the HTTP data and the decoded version.
 
     Args:
         packet_contents (str): String containing contents of packet
 
     Returns:
-        Union[dict,None]: Dictionary containing the data and the decoded form. If
+        Optional[dict]: Dictionary containing the data and the decoded form. If
         the length of the data is 0 there is no HTTP data in the packet, so the
         return is None.
     """
