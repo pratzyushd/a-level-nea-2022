915cc905f72362005b656a2e1a25b82c62dec4d4
Pratyush Dwivedi <pratzyushd@gmail.com>
2022-03-28 13:35:33 +0400

    Add comments to app.py explaining what each route does

--- app.py
+++ app.py
@@ -36,10 +36,12 @@ def first_page() -> None:
     time.sleep(0.25)
     return render_template("request.html")
 
+# Loading page between request being made and display of transaction contents
 @app.route('/loading')
 def load_data() -> None:
     return render_template("loading.html")
 
+# Page to display transaction contents with explanations
 @app.route("/a-level")
 def a_level() -> None:
     file_to_find = session.get("time_value", None) + ".txt"
@@ -62,17 +64,23 @@ def a_level() -> None:
     return render_template("a-level.html", eth = eth_headers, ip = ip_headers,
             tcp = tcp_headers, http = http_data, client_ip = client_ip)
 
+# Introduction page for RSA encryption functionality
 @app.route('/rsa/intro')
 def rsa_intro() -> None:
     session["prime_1"] = rsa.get_prime()
     session["prime_2"] = rsa.get_prime()
     return render_template("rsa/introduction.html")
 
+# Generating prime numbers for RSA encryption functionality
 @app.route("/rsa/prime-gen")
 def rsa_prime_generation() -> None:
     return render_template("/rsa/key_gen.html", prime_1 = session["prime_1"],
     prime_2 = session["prime_2"])
 
+# If GET request: showing the message page, with information about how RSA
+# encryption works
+# If POST request: take the message andthe two prime numbers, and encrypt and
+# decrypt it, and store the messages in the session variable
 @app.route("/rsa/message", methods = ["POST", "GET"])
 def rsa_message() -> "None":
     if request.method == "POST":
@@ -87,6 +95,7 @@ def rsa_message() -> "None":
     else:
         return render_template("/rsa/message.html")
 
+# Display the encrypted and decrypted message to the user
 @app.route("/rsa/encryption-decryption")
 def rsa_encrypt_decrypt() -> None:
     return render_template("/rsa/encrypt-decrypt.html",
