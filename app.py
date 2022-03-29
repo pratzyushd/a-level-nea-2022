98f189596ebcfaf3b7afe23e0be7c7321929efb9
Pratyush Dwivedi <pratzyushd@gmail.com>
2022-03-29 17:26:02 +0400

    Add error prevention
    Prevent user from trying to access a page that they shouldn't already be
    able to access e.g. can't access transaction display page without having
    started the sniffer / made a request.

--- app.py
+++ app.py
@@ -20,51 +20,86 @@ with open('secret_key.txt', 'r') as f:
 
 # Route for the landing page i.e. the first page you see when you land on the
 # website.
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def landing_page() -> None:
+    if request.method == "POST":
+        session["sniffer_called"] = True
+        return redirect(url_for("request_page"))
+    session["sniffer_called"] = False
     return render_template("landing_page.html")
 
 # The page that makes the request i.e. starts the sniffer
-@app.route("/request", methods = ["POST","GET"])
-def first_page() -> None:
-    current_time = str(time.time_ns())
-    client_ip = request.remote_addr
-    # Use subprocess library to call the sniffer script
-    output_from_sniffer_call = sc.Popen(["sudo", "./run-sniffer.sh", "-f",
-        current_time, "-a", client_ip], stdout = sc.PIPE,
-        stderr = sc.PIPE)
-    session["time_value"] = current_time
-    session["client_ip"] = client_ip
-    time.sleep(0.25)
-    return render_template("request.html")
+@app.route("/request")
+def request_page() -> None:
+    # Try, except, else block. Prevents users from trying to make a request
+    # without having actually initialised the sniffer in the first place.
+    try:
+        if not session["sniffer_called"]:
+            return redirect(url_for("landing_page"))
+    except KeyError:
+        session["sniffer_called"] = False
+        return redirect(url_for("landing_page"))
+    # If the user has already called the sniffer, execute this block
+    else:
+        current_time = str(time.time_ns())
+        client_ip = request.remote_addr
+        # Use subprocess library to call the sniffer script
+        output_from_sniffer_call = sc.Popen(["sudo", "./run-sniffer.sh", "-f",
+            current_time, "-a", client_ip], stdout = sc.PIPE, stderr = sc.PIPE)
+        session["time_value"] = current_time
+        session["client_ip"] = client_ip
+        time.sleep(0.25)
+        return render_template("request.html")
 
 # Loading page between request being made and display of transaction contents
 @app.route('/loading')
 def load_data() -> None:
-    return render_template("loading.html")
+    # Try, except, else block. Prevents users from trying to get to this
+    # page without having actually initialised the sniffer in the first place.
+    try:
+        if not session["sniffer_called"]:
+            return redirect(url_for("landing_page"))
+    except KeyError:
+        session["sniffer_called"] = False
+        return redirect(url_for("landing_page"))
+    # If the user has already called the sniffer, execute this block
+    else:
+        return render_template("loading.html")
 
 # Page to display transaction contents with explanations
 @app.route("/a-level")
 def a_level() -> None:
-    file_to_find = session.get("time_value", None) + ".txt"
-    with open("output_files/"+file_to_find, "r") as f:
-        contents = f.read().splitlines()
+    # Try, except, else block. Prevents users from trying to see the
+    # contents of a transaction without having actually initialised the sniffer
+    # to begin with.
+    try:
+        if not session["sniffer_called"]:
+            return redirect(url_for("landing_page"))
+    except KeyError:
+        session["sniffer_called"] = False
+        return redirect(url_for("landing_page"))
+    # If the user has already called the sniffer, execute this block
+    else:
+        file_to_find = session.get("time_value", None) + ".txt"
+        with open("output_files/"+file_to_find, "r") as f:
+            contents = f.read().splitlines()
 
-    # Go through contents of file and decode each relevant header
-    eth_headers = []
-    ip_headers = []
-    tcp_headers = []
-    http_data = []
-    for i in range (0, len(contents), 4):
-        eth_headers.append(decoding.decode_eth_header(contents[i]))
-        ip_headers.append(decoding.decode_ip_header(contents[i+1]))
-        tcp_headers.append(decoding.decode_tcp_header(contents[i+2]))
-        http_data.append(decoding.decode_http_data(contents[i+3]))
+        # Go through contents of file and decode each relevant header
+        eth_headers = []
+        ip_headers = []
+        tcp_headers = []
+        http_data = []
+        for i in range (0, len(contents), 4):
+            eth_headers.append(decoding.decode_eth_header(contents[i]))
+            ip_headers.append(decoding.decode_ip_header(contents[i+1]))
+            tcp_headers.append(decoding.decode_tcp_header(contents[i+2]))
+            http_data.append(decoding.decode_http_data(contents[i+3]))
 
-    client_ip = session["client_ip"]
-    # Pass the various headers and packet data to the template
-    return render_template("a-level.html", eth = eth_headers, ip = ip_headers,
-            tcp = tcp_headers, http = http_data, client_ip = client_ip)
+        client_ip = session["client_ip"]
+        # Pass the various headers and packet data to the template
+        return render_template("a-level.html", eth = eth_headers,
+        ip = ip_headers, tcp = tcp_headers, http = http_data,
+        client_ip = client_ip)
 
 # Introduction page for RSA encryption functionality
 @app.route('/rsa/intro')
@@ -95,10 +130,28 @@ def rsa_message() -> "None":
         session["plaintext"] = decrypted
         return redirect(url_for("rsa_encrypt_decrypt"))
     else:
-        return render_template("/rsa/message.html")
+        # Try except block. If there are no prime number values in the session,
+        # user hasn't visited the key generation page, so make them visit that
+        # page to generate prime numbers.
+        try:
+            session["prime_1"]
+            session["prime_2"]
+        except KeyError:
+            return redirect(url_for("rsa_key_generation"))
+        else:
+            return render_template("/rsa/message.html")
 
 # Display the encrypted and decrypted message to the user
 @app.route("/rsa/encryption-decryption")
 def rsa_encrypt_decrypt() -> None:
-    return render_template("/rsa/encrypt-decrypt.html",
-    plaintext = session["plaintext"], ciphertext = session["ciphertext"])
+    # Try except block. If there is no ciphertext or plaintext in the session,
+    # user hasn't visited the message page, so make them visit that page to
+    # get a message from them.
+    try:
+        session["ciphertext"]
+        session["plaintext"]
+    except KeyError:
+        return redirect(url_for("rsa_message"))
+    else:
+        return render_template("/rsa/encrypt-decrypt.html",
+        plaintext = session["plaintext"], ciphertext = session["ciphertext"])
